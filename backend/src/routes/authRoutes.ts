51c627e90462689c969b628924aa068927c0950f
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-09-30 03:00:14 +0200

    excluded .js files

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import { signup, login } from "../controllers/authController";
 
 const router = express.Router();
